a399cff7f84ce56a4bacf9c32296f4c681543450
Ciprian Ionita <icipcon@gmail.com>
2025-02-23 14:33:26 +0200

    modified portfolio section and added new portfolio items

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import React from "react";
-import Link from "next/link";
+import React, { useState } from "react";
 import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
 import Project from "./project";
@@ -10,15 +9,39 @@ import { useSectionInView } from "@/lib/hook";
 export default function Projects() {
     const { ref } = useSectionInView("Projects", 0.5);
 
+    const [visibleItems, setVisibleItems] = useState(3); // Show first 3 items without class
+
+    const handleLoadMore = () => {
+        setVisibleItems((prev) => Math.min(prev + 3, projectsData.length)); // Increase count
+    };
+
+    const allVisible = visibleItems >= projectsData.length; // Check if all are visible
+
     return (
         <section id="projects" className="scroll-mt-28 mb-28" ref={ref}>
             <SectionHeading>My Latest Projects</SectionHeading>
-            <div>
-                {projectsData.map((project, index) => (
-                    <React.Fragment key={index}>
-                        <Project {...project} />
-                    </React.Fragment>
-                ))}
+            {projectsData.map((project, index) => (
+                <article
+                    key={index}
+                    className={`group mb-3 sm:mb-8 last:mb-0 ${
+                        index < visibleItems ? "" : "hidden"
+                    }`}
+                >
+                    <Project {...project} />
+                </article>
+            ))}
+            <div className="mt-4 text-center">
+                <button
+                    onClick={handleLoadMore}
+                    disabled={allVisible}
+                    className={`px-7 py-3 bg-white bg-opacity-10 text-white rounded-full transition-all ${
+                        allVisible
+                            ? "disabled:scale-100 disabled:bg-opacity-65 cursor-not-allowed"
+                            : "hover:scale-110 hover:bg-gray-950 focus:scale-110 active:scale-105"
+                    }`}
+                >
+                    Load More
+                </button>
             </div>
         </section>
     );
--- lib/data.ts
+++ lib/data.ts
@@ -1,6 +1,9 @@
 import imgNattyPell from "@/public/nattypell.png";
 import imgKingSoftware from "@/public/king-software.png";
 import imgATBConcessions from "@/public/atbconcessions.png";
+import imgEsthemax from "@/public/esthemax.png";
+import imgTSA from "@/public/torontostrikingacademy.png";
+import imgLiquidBeauty from "@/public/liquidbeauty.png";
 
 export const links = [
     {
@@ -63,6 +66,7 @@ export const projectsData = [
             "I built an ecommerce store for a local handmade leather goods brand, integrating payment and shipping services. First I designed the website in Figma and then developed it.",
         tags: ["WordPress", "WooCommerce", "Divi", "CSS", "Figma"],
         imageUrl: imgNattyPell,
+        url: "https://nattypell.ro",
     },
     {
         title: "King Software",
@@ -70,6 +74,7 @@ export const projectsData = [
             "A WooCommerce store I built for a software license provider. Focused on a clean, modern, and intuitive design, it highlights products, uses clear call-to-actions, and builds trust with detailed guides and partner logos.",
         tags: ["WordPress", "WooCommerce", "PHP", "CSS", "Figma"],
         imageUrl: imgKingSoftware,
+        url: "https://king-software.ciprianionita.net",
     },
     {
         title: "ATB Concessions",
@@ -77,6 +82,31 @@ export const projectsData = [
             "Designed a static HTML website from scratch with Figma and developed it using Astro JS framework for optimal performance and user experience.",
         tags: ["Astro", "HTML", "CSS", "JavaScript", "Figma"],
         imageUrl: imgATBConcessions,
+        url: "https://atbconcessions.co.uk",
+    },
+    {
+        title: "Esthemax",
+        description:
+            "Developed an e-commerce website using WooCommerce and Elementor. I tailored the design to match the client's branding and implemented a functionality for easy product bundles creation.",
+        tags: ["WordPress", "WooCommerce", "PHP", "CSS", "JavaScript"],
+        imageUrl: imgEsthemax,
+        url: "https://esthemax.ca",
+    },
+    {
+        title: "Toronto Striking Academy",
+        description:
+            "Built a WordPress + WooCommerce site for a MMA gym. Features: custom visuals, class schedules, memberships, merchandise, and a blog.",
+        tags: ["WordPress", "WooCommerce", "PHP", "CSS", "JavaScript"],
+        imageUrl: imgTSA,
+        url: "https://www.torontostrikingacademy.com",
+    },
+    {
+        title: "Liquid Beauty",
+        description:
+            "Modified a WordPress website for a medical spa. I enhanced the site with a before-and-after gallery, FAQ accordions, custom contact forms, and subtle animations. Also fixed the accessibility issues to make it legally compliant.",
+        tags: ["WordPress", "PHP", "CSS", "JavaScript"],
+        imageUrl: imgLiquidBeauty,
+        url: "https://www.liquidbeauty.com",
     },
 ] as const;
 
